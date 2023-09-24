58d4004824f14fd118129b62d199c180c49f8099
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-09-24 13:46:22 +0200

    allowing to 'put' a slug

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ using Newtonsoft.Json.Linq;
 using System.Text.Json;
 using System.Collections;
 using System.Net;
+using System.ComponentModel;
 
 public class Handler 
 {
@@ -20,6 +21,8 @@ public class Handler
     private string databaseId = Environment.GetEnvironmentVariable("APPWRITE_DATABASE_ID");
     private bool log = Environment.GetEnvironmentVariable("LOG_REQUESTS") == "true" ? true : false;
     private string defaultUrl = Environment.GetEnvironmentVariable("APPWRITE_DEFAULTURL");
+
+    private int resultstatus = 200;
 
     // This is your Appwrite function
     // It is executed each time we get a request
@@ -34,13 +37,77 @@ public class Handler
 
         switch (Context.Req.Method) {   
             case "GET":
-                return Context.Res.Redirect(await Get(Context),302);
+                return Context.Res.Redirect(await Get(Context),resultstatus);
+            case "POST":
+                return Context.Res.Send(await Post(Context),resultstatus);
             default:
                 return Context.Res.Json(new Dictionary<string, object>()
                     {
                         { "message", "This is not the endpoint you were looking for!" },
                         { "timestamp", DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff zzz") }
                     });
+        }
+    }
+
+    private async Task<string> Post(DotNetRuntime.RuntimeContext context)
+    {
+        // take the "slug" and "destination" url from the request body and store it in the appwrite collection specified by the environment variables
+        // if the slug already exists return an error
+        // if the slug does not exist create a new document with the slug and destination url
+        // return the new document as json
+        try
+        {
+            Client client = new Client();
+            client
+                .SetEndpoint(endpoint)
+                .SetProject(projectId)
+                .SetKey(apiKey)
+                .SetSelfSigned(true)
+            ;
+
+            // extract the slug and destination from the request body
+            var body = context.Req.BodyJson<Dictionary<string, object>>();
+            var slug = body["slug"].ToString();
+            var destination = body["destination"].ToString();
+
+            if (log) 
+            {
+                context.Log("create slug: " + slug);
+                context.Log("for destination: " + destination);
+            }
+            
+            Databases databases = new Databases(client);
+            if (log) context.Log("database connected");
+
+            // check if the slug already exists
+            var documentList = await databases.ListDocuments(collectionId: collectionId, databaseId: databaseId, filters: new List<string> { $"slug={slug}"});
+            if (log) context.Log("documents: " + documentList.Documents.Count);
+            if (documentList.Documents.Count > 0)
+            {
+                // if the slug already exists return an error
+                resultstatus = 400;
+                return "slug already exists";
+            }
+            else
+            {
+                // if the slug does not exist create a new document with the slug and destination url
+                var document = await databases.CreateDocument(collectionId: collectionId, databaseId: databaseId, data: new Dictionary<string, object> { { "slug", slug }, { "destination", destination } });
+                if (log) context.Log("document created");
+                if (log) context.Log("document id: " + document.Id);
+                if (log) context.Log("document data: " + document.Data);
+                if (document.Id == null)
+                {
+                    resultstatus = 500;
+                    return "error creating document";
+                }
+                resultstatus = 201;
+                return document.Data.ToString();
+            }
+        }
+        catch (Exception e)
+        {
+            resultstatus = 500;
+            context.Log(e.Message);
         }
     }
     private async Task<string> Get(DotNetRuntime.RuntimeContext Context)
@@ -76,10 +143,9 @@ public class Handler
             //get the first element from documentList and return it as string queries: new List<string> { $"slug={slug}"}
             if (documentList.Documents.Count > 0)
             {
-                 Context.Log("destination 1 of " + documentList.Documents.Count );
+                if (log) Context.Log("destination 1 of " + documentList.Documents.Count );
                 var firstDocument = documentList.Documents.First();
                 return firstDocument.Data.GetValueOrDefault("destination").ToString();
-                
             }
             else
             {
@@ -88,6 +154,7 @@ public class Handler
         }
         catch (Exception e)
         {
+            resultstatus = 500;
             Context.Log(e.Message);
         }
 
