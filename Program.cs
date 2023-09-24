1c42ff7b03c34e2293f719e016578b5e83a26772
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-09-24 19:42:06 +0200

    secure the creation with

--- Program.cs
+++ Program.cs
@@ -70,7 +70,7 @@ public class Handler
             JObject bodyjson = JObject.Parse(context.Req.BodyRaw);
 
             //validate a api key is provided
-            if (bodyjson["key"] != clientApiKey)
+            if (string)bodyjson["key"] != clientApiKey)
             {
                 resultstatus = 401;
                 return "no api key provided";
